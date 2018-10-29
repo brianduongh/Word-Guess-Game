21e5aeeca729ab836688be096aa03d688919bcaa
Brian Duong <brianduongh@gmail.com>
2018-10-29 18:00:06 -0500

    added css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,7 @@ var $wrongLetters = document.getElementById("wrong-letters");
 var $guessesLeft = document.getElementById("guesses-left");
 var $wins = document.getElementById("wins");
 var $losses = document.getElementById("losses");
+var $body = document.getElementById("body");
 
 // Variables
 var colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
@@ -12,10 +13,11 @@ var chosenWord = colors[index];
 var underScore = [];
 var guessedLetters = [];
 var wrongLetters = [];
-var guessesLeft = 5;
+var guessesLeft = 7;
 var wins = 0;
 var losses = 0;
 var gameFinished = false;
+var audio = new Audio("./assets/audio/readingRainbow.mp3");
 
 // Testing
 console.log(chosenWord);
@@ -71,12 +73,54 @@ function checkGuess(letter) {
 function checkAnswer() {
   if (underScore.join("") == chosenWord) {
     wins++;
+    changeColor();
     resetWord();
+    audio.play();
   } else if (guessesLeft == 0) {
     losses++;
     resetWord();
   };
   updateDomElements();
+}
+// Updates colors and background based off answer
+function changeColor() {
+  switch (underScore.join("")) {
+    case "red":
+      $body.style.background = '#FF0000';
+      $body.style.color = '#00FF00';
+      $body.style.transition = "all 1s";
+      break;
+    case "orange":
+      $body.style.background = '#FF7F00';
+      $body.style.color = '#0000FF';
+      $body.style.transition = "all 1s";
+      break;
+    case "yellow":
+      $body.style.background = '#FFFF00';
+      $body.style.color = '#9400D3';
+      $body.style.transition = "all 1s";
+      break;
+    case "green":
+      $body.style.background = '#00FF00';
+      $body.style.color = '#FF0000';
+      $body.style.transition = "all 1s";
+      break;
+    case "blue":
+      $body.style.background = '#0000FF';
+      $body.style.color = '#FF7F00';
+      $body.style.transition = "all 1s";
+      break;
+    case "indigo":
+      $body.style.background = '#4B0082';
+      $body.style.color = '#FF7F00';
+      $body.style.transition = "all 1s";
+      break;
+    case "violet":
+      $body.style.background = '#9400D3';
+      $body.style.color = '#FFFF00';
+      $body.style.transition = "all 1s";
+      break;
+  }
 }
 
 // Reset game if user wins or losses
@@ -84,12 +128,13 @@ function resetWord() {
   index = Math.floor(Math.random() * colors.length);
   chosenWord = colors[index];
   underScore = [];
-  generateUnderScore();
   guessesLeft = 5;
   guessedLetters = [];
   wrongLetters = [];
-  updateDomElements();
   console.log(chosenWord);
+  updateDomElements();
+  generateUnderScore();
+  audio.play();
 }
 
 // Capture key event
